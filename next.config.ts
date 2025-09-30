96ee795ced9f54ef91d9d379005abceb9e6c996d
Owenalbert606 <owenalbert606@gmail.com>
2025-09-30 18:59:36 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,34 @@
 
 
 
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
