931c30c8b125419fb86b3b15248aa8835044bc4a
Owenalbert606 <owenalbert606@gmail.com>
2025-10-11 23:20:44 +0300

    h

--- components/front-end/movie-section.tsx
+++ components/front-end/movie-section.tsx
@@ -160,7 +160,7 @@ export function MovieSection({ title, movies }: MovieSectionProps) {
 
       <div
         id={`scroll-${title.replace(/\s+/g, "-")}`}
-        className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-4 overflow-x-auto scrollbar-hide pb-4"
+        className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4 overflow-x-auto scrollbar-hide pb-4"
         style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
       >
         {movies.map((movie) => (
